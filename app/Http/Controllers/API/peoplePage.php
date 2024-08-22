1d776ae3f92cdbb40b3b7fb0be81f57e1739a19a
multydo <o9danieltra@gmail.com>
2024-08-22 18:31:55 +0300

    people response and docs

--- app/Http/Controllers/API/peoplePage.php
+++ app/Http/Controllers/API/peoplePage.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\getUserId;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class peoplePage extends Controller
 {
@@ -17,19 +18,35 @@ class peoplePage extends Controller
 
             $userTable = $userId . "_people";
 
+            try{$follow_data = DB::table('users as u')
+                    ->join($userTable . ' as f', 'u.id', '=', 'f.user_f_id')
+                    ->where('f_status', 'following')
+                    ->select('u.fname', 'u.lname', 'u.id')
+                    ->get()
+                    ->toArray();
+            }catch(Throwable $e){
+                return response()->json([
+                    "message"=> $e->getMessage()
+                ],500);
+            }
+            
 
-            $follow_data = DB::table('users as u')
-    ->join($userTable . ' as f', 'u.id', '=', 'f.user_f_id')
-    ->where('f_status', 'following')
-    ->select('u.fname', 'u.lname', 'u.id')
-    ->get()
-    ->toArray();
+            $status = true;
+            $text = "users fownd";                
 
-if (empty($follow_data)) {
-    $follow_data[] = '0';
-}
+            if (empty($follow_data)) {
+                $follow_data[] = '0';
+                $status = false;
+                $text = "no users fownd";  
+            }
+            return response()->json([
+                "message"=>$text,
+                "status"=>$status,
+                "data" => $follow_data
+            ],200);
 
-dd($follow_data);
+
+
         }else{
             return response()->json([
                 "message"=>"user not found or token is broken"
@@ -54,23 +71,46 @@ dd($follow_data);
             'user_f_id' => $userFId,
         ]);
 
-        // Update following_num in users_follower
-        $result21 = DB::table('followers_users')
-            ->where('userId', $userId)
-            ->increment('following_num', 1);
+       
+       
 
-        // Second Insert Operation
+        try{
+            $result21 = DB::table('followers_users')
+                ->where('userId', $userId)
+                ->increment('following_num', 1);
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
         $f_status3 = "follower";
-        DB::table($userFTable)->insert([
+        try{DB::table($userFTable)->insert([
             'owner_id' => $userFId,
             'f_status' => $f_status3,
             'user_f_id' => $userId,
-        ]);
+        ]);}catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);}
 
-        // Update followers_num in users_follower
-        $result31 = DB::table('followers_users')
-            ->where('userId', $userFId)
-            ->increment('followers_num', 1);
+    
+        
+        
+
+        try{
+            $result31 = DB::table('followers_users')
+                ->where('userId', $userFId)
+                ->increment('followers_num', 1);
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
+
+        
+        return response()->json([
+            "message"=>"userd followed sucsesfully"
+        ],200);
         
     }
 
@@ -84,53 +124,88 @@ dd($follow_data);
         $userTable = $userId."_people";
         $userFTable = $userFId."_people";
 
-        DB::table($userTable)->where('user_f_id', $userFId)->delete();
-        DB::table($userFTable)->where('user_f_id', $userId)->delete();
+        try{
+            DB::table($userTable)->where('user_f_id', $userFId)->delete();
+            DB::table($userFTable)->where('user_f_id', $userId)->delete();
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
 
-        // Select owner_id counts
-        $num_row_p = DB::table($userTable)->count();
-        $num_row_f_p = DB::table($userFTable)->count();
+        
 
-        // Update following_num in users_follower for the user_id
-        DB::table('followers_users')
-        ->where('userId', $userId)
-       ->update(['following_num' => $num_row_p]);
+        try{
+            $num_row_p = DB::table($userTable)->count();
+            $num_row_f_p = DB::table($userFTable)->count();
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
+        
 
-        // Update following_num in users_follower for the user_f_id
-        DB::table('followers_users')
-        ->where('userId', $userFId)
-        ->update(['following_num' => $num_row_f_p]);
+         try{
+            DB::table('followers_users')
+                ->where('userId', $userId)
+                ->update(['following_num' => $num_row_p]);
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
+       
+
+         try{
+            DB::table('followers_users')
+                ->where('userId', $userFId)
+                ->update(['following_num' => $num_row_f_p]);
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
+        return response()->json([
+            "message"=>"user unfollowed"
+
+        ],200);
     }
 
     public function searchPeople(Request $request){
-         $token = $request -> bearerToken();
+        $token = $request -> bearerToken();
         $get_user_id = new getUserId;
         $userId = $get_user_id -> getId($token);
         $requestData = $request->json()->all();
         
         $userTable = $userId."_people";
         $gues = $requestData['search'];
+        try{
+            $search_data = DB::table('users as u')
+                //->join('followers_users as f', 'u.id', '=', 'f.userId')
+                ->leftJoin($userTable . ' as p', 'p.user_f_id', '=', 'u.id')
+                ->select( 'u.fname', 'u.lname', 'p.f_status')
+                ->where(function($query) use ($gues) {
+            $query->where('u.fname', 'like', $gues . '%')
+                ->orWhere('u.lname', 'like', $gues . '%')
+                ->orWhere('u.fname', '=', $gues)
+                ->orWhere('u.lname', '=', $gues)
+                ->orWhere('u.username', '=', $gues);
+            })
+                ->where('u.id', '<>', $userId)
+                ->get()
+                ->toArray();
+        }catch(Throwable $e){
+            return response()->json([
+                "message"=> $e->getMessage()
+            ],500);
+        }
+        
 
-        $search_data = DB::table('users as u')
-    //->join('followers_users as f', 'u.id', '=', 'f.userId')
-    ->leftJoin($userTable . ' as p', 'p.user_f_id', '=', 'u.id')
-    ->select( 'u.fname', 'u.lname', 'p.f_status')
-    ->where(function($query) use ($gues) {
-        $query->where('u.fname', 'like', $gues . '%')
-              ->orWhere('u.lname', 'like', $gues . '%')
-              ->orWhere('u.fname', '=', $gues)
-              ->orWhere('u.lname', '=', $gues)
-              ->orWhere('u.username', '=', $gues);
-    })
-    ->where('u.id', '<>', $userId)
-    ->get()
-    ->toArray();
+        // Check if there are results
+        if (empty($search_data)) {
+            $search_data[] = '0';
+        }
 
-// Check if there are results
-if (empty($search_data)) {
-    $search_data[] = '0';
-}
-
-return response()->json([$search_data]);
+        return response()->json([$search_data]);
     }
 }
